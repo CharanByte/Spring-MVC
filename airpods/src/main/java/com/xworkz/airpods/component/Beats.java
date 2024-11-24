203618d2be51bc773e8bc98e2db4244aad6f91e0
CharanByte <charan7812@gmail.com>
2024-11-24 21:48:11 +0530

    Creating Components using Spring

---
+++ airpods/src/main/java/com/xworkz/airpods/component/Beats.java
@@ -0,0 +1,10 @@
+package com.xworkz.airpods.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Beats {
+    Beats(){
+        System.out.println("running Beats const");
+    }
+}
---
+++ airpods/src/main/java/com/xworkz/airpods/component/OnePlus.java
@@ -0,0 +1,10 @@
+package com.xworkz.airpods.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class OnePlus {
+    OnePlus(){
+        System.out.println("running Oneplus const");
+    }
+}
